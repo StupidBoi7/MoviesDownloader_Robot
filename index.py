183eacd743a87d2032c475016442a8d49c9cfb84
StupidBoi7 <126649764+StupidBoi7@users.noreply.github.com>
2023-07-10 14:18:45 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://movies-downloader-robot.vercel.app"
+URL = "https://movies-downloading-bot-v2.vercel.app"
 bot = Bot(TOKEN)
 
 
